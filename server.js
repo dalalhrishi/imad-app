0c7164d0f1c4a6f24c4155494df935d7578f9483
dalalhrishi <30819863+dalalhrishi@users.noreply.github.com>
2017-08-08 17:21:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one',function(req,res){
+app.get('/article-one', function(req, res){
     res.send('Article 1 is requested and served here');
 }
     );
